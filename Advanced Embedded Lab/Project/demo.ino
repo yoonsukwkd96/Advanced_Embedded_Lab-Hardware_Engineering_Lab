be28951853511b3083885132892ee4c26f645684
Muhammad Rohail Usman <56233774+Rohail999@users.noreply.github.com>
2022-05-30 04:01:27 +0200

    Update demo.ino

--- Advanced Embedded Lab/Project/demo.ino
+++ Advanced Embedded Lab/Project/demo.ino
@@ -22,8 +22,11 @@ unsigned long previousMillis = 0;
 
 int count = 0;
 int sensorPin = A5; // Declare the input pin here
-// int tempPin = A7;
-
+/* int tempPin = A0; // Declaring Input Pin for Temp
+int Vo;
+float R1 = 10000;
+float logR2, R2, T, Tc;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07; */
 
 // Serial output in 9600 baud
 void setup()
@@ -133,9 +136,19 @@ void loop()
     //--------End light sensor-----------------
 
     //------------Temperature sensor-----------
+    //
+ /*
+  Vo = analogRead(tempPin);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  Tc = T - 273.15;
+  
 
-
-
+  Serial.print("Temperature: "); 
+  Serial.print(Tc);
+  Serial.println(" C");   
+  */
 
 
 
